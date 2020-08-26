eb2f0362e2177bd25b01fd2440d1377ede0f3e1c
gogorikidze <git.gogorikidze@gmail.com>
2020-08-26 17:10:35 +0400

    optimised to support higher resolutions. now there's better blank space

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 include('tracer.php');
 $frame = $_GET['frame'];
 
+if(true){
+  $frame += 1;
+  header('refresh:0.1; url=index.php?frame='.$frame);
+}
+
 $tri = new Triangle(
-  new Vec3(-2 - $frame/200, -2 + $frame/200, 5),
-  new Vec3(2 + $frame/200, -2 - $frame/200, 5),
-  new Vec3(0 - $frame/200, 2 + $frame/200, 5)
+  new Vec3(-2 - $frame/20, -2 + $frame/20, 5),
+  new Vec3(2 + $frame/20, -2 - $frame/20, 5),
+  new Vec3(0 - $frame/20, 2 + $frame/20, 5)
 );
-$camera = new Camera(new Vec3(0,0,0), 30, 50, 3, 3);
+$camera = new Camera(new Vec3(0,0,0), 160, 280, 3, 10);
 $camera->renderTriangle($tri);
 
-if(true){
-  echo 'frame:'.$frame;
-  $frame += 1;
-  header('refresh:0.001; url=index.php?frame='.$frame);
-}
+
 ?>
