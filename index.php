e3840756eccac77a2142a684646f3cd663f16dd6
saba gogorika <levan@gogorikidze.com>
2020-08-27 14:40:04 +0400

    optimised fps by creating buffer and frustum in SESSION

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
-include('tracer.php');
+include('three.php');
 $frame = $_GET['frame'];
 
 $scene = new Scene();
+
+$mesh = new Mesh();
 $tri = new Triangle(
   new Vec3(-2 - $frame/20, -2 + $frame/20, 5),
   new Vec3(2 + $frame/20, -2 - $frame/20, 5),
   new Vec3(0 - $frame/20, 2 + $frame/20, 5),
   "#"
 );
-$scene->addObject($tri);
-$camera = new Camera(new Vec3(0,0,0), 156/4, 319/4, 3, 20/4);
+$tri2 = new Triangle(
+  new Vec3(-5 - $frame/20, -5 + $frame/20, 5),
+  new Vec3(-1 + $frame/20, -5 - $frame/20, 5),
+  new Vec3(-3 - $frame/20, -1 + $frame/20, 5),
+  "0"
+);
+$mesh->addTriangle($tri);
+$mesh->addTriangle($tri2);
+$scene->addObject($mesh);
+
+$camera = new Camera(new Vec3(0,0,0), 156/4, 318/4, 3, 20/4);
 $camera->render($scene, $frame);
-
-
 ?>
