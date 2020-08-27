f80141ecccf2f13e7dddec9746f701bc3d42248d
gogorikidze <git.gogorikidze@gmail.com>
2020-08-27 16:34:28 +0400

    ?fixed a bug which throws divide by zero error

--- index.php
+++ index.php
@@ -5,22 +5,26 @@ $frame = $_GET['frame'];
 $scene = new Scene();
 
 $mesh = new Mesh();
-$tri = new Triangle(
-  new Vec3(-2 - $frame/20, -2 + $frame/20, 5),
-  new Vec3(2 + $frame/20, -2 - $frame/20, 5),
-  new Vec3(0 - $frame/20, 2 + $frame/20, 5),
+$mesh->addTriangle(new Triangle(
+  new Vec3(0.5, 0, 0.5),
+  new Vec3(0, 0, 0),
+  new Vec3(0.5, 1, 0.5),
+  "$"
+));
+$mesh->addTriangle(new Triangle(
+  new Vec3(1, 0, 0),
+  new Vec3(0.5, 0, 0.5),
+  new Vec3(0.5, 1, 0.5),
   "#"
-);
-$tri2 = new Triangle(
-  new Vec3(-5 - $frame/20, -5 + $frame/20, 5),
-  new Vec3(-1 + $frame/20, -5 - $frame/20, 5),
-  new Vec3(-3 - $frame/20, -1 + $frame/20, 5),
+));
+$mesh->addTriangle(new Triangle(
+  new Vec3(0, 0, 0),
+  new Vec3(1, 0, 0),
+  new Vec3(0.5, 0, 0.5),
   "0"
-);
-$mesh->addTriangle($tri);
-$mesh->addTriangle($tri2);
+));
 $scene->addObject($mesh);
 
-$camera = new Camera(new Vec3(0,0,0), 156/4, 318/4, 3, 20/4);
+$camera = new Camera(new Vec3(0,0,-5), 133, 239, 3, 30, false);
 $camera->render($scene, $frame);
 ?>
