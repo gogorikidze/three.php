145ac67486118595920480e0e77f3b04db563863
saba gogorika <levan@gogorikidze.com>
2020-08-27 19:35:53 +0400

    added meshes and geometries with faces and vertices

--- index.php
+++ index.php
@@ -4,27 +4,21 @@ $frame = $_GET['frame'];
 
 $scene = new Scene();
 
-$mesh = new Mesh();
-$mesh->addTriangle(new Triangle(
-  new Vec3(0.5, 0, 0.5),
-  new Vec3(0, 0, 0),
-  new Vec3(0.5, 1, 0.5),
-  "$"
-));
-$mesh->addTriangle(new Triangle(
-  new Vec3(1, 0, 0),
-  new Vec3(0.5, 0, 0.5),
-  new Vec3(0.5, 1, 0.5),
-  "#"
-));
-$mesh->addTriangle(new Triangle(
-  new Vec3(0, 0, 0),
-  new Vec3(1, 0, 0),
-  new Vec3(0.5, 0, 0.5),
-  "0"
-));
-$scene->addObject($mesh);
+//sets up geometry
+$geometry = new Geometry();
 
-$camera = new Camera(new Vec3(0,0,-5), 133, 239, 3, 30, false);
+$geometry->addVertex(new Vec3(0,0,0));
+$geometry->addVertex(new Vec3(1,0,0));
+$geometry->addVertex(new Vec3(0.5,0,0.5));
+$geometry->addVertex(new Vec3(0.5,1,0.5));
+
+$geometry->addFace(new Vec3(0,1,2));
+$geometry->addFace(new Vec3(0,2,3));
+$geometry->addFace(new Vec3(1,2,3));
+
+$mesh = new Mesh($geometry, '#');
+$scene->addMesh($mesh);
+
+$camera = new Camera(new Vec3(0,0,-5), 133/2, 239/2, 3, 30/2, false);
 $camera->render($scene, $frame);
 ?>
