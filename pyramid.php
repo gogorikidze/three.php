9e24d316bee9b4852afb2f91692949c0c2370194
gogorikidze <git.gogorikidze@gmail.com>
2020-08-30 16:30:28 +0400

    added stats - fps and frametimes

--- pyramid.php
+++ pyramid.php
@@ -10,9 +10,9 @@ $geometry = new Geometry();
 $geometry->addVertex(new Vec3(-1,0,-1));
 $geometry->addVertex(new Vec3(1,0,-1));
 $geometry->addVertex(new Vec3(0,0,1));
-$geometry->addVertex(new Vec3(0,1,0));
+$geometry->addVertex(new Vec3(0,2,0));
 
-$geometry->rotate($frame*5*pi()/180,0,0);
+$geometry->rotate($frame*7*pi()/180,0,0);
 
 $geometry->addFace(new Face(0,3,1,'$'));
 $geometry->addFace(new Face(1,3,2,'#'));
@@ -22,6 +22,6 @@ $geometry->addFace(new Face(0,1,2,'p'));
 $mesh = new Mesh($geometry, '#');
 $scene->addMesh($mesh);
 
-$camera = new Camera(new Vec3(0,2,5), 133/4, 239/4, 3, 30/4, false);
-$camera->render($scene, $frame);
+$camera = new Camera(new Vec3(0,1,5), 133/2, 239/2, 3, 60/2, true);
+$camera->render($scene, $frame, "pyramid.php");
 ?>
