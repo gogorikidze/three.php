a91be32e39e88ac18e3c28f832c57bc7b6c9d24b
saba gogorika <levan@gogorikidze.com>
2020-09-03 02:19:21 +0400

    fixed divide by zeo in barycentric coords algorythm

--- index.php
+++ index.php
@@ -3,24 +3,15 @@ include('three.php');
 $frame = $_GET['frame'];
 
 $scene = new Scene();
-$camera = new RayCamera(new Vec3(0,0.5,5), 100, 100, 3, 30, false);
+$renderer = new HTMLRenderer(true);
+$camera = new RasterCamera(new Vec3(0,0,5), 240/6, 320/6, 3, 60/6, false);
 
-$geometry = new Geometry();
-$geometry->addVertex(new Vec3(0,1,0));
-$geometry->addVertex(new Vec3(-1,0,0));
-$geometry->addVertex(new Vec3(1,0,0));
-$geometry->addFace(new Face(0,1,2,'$'));
+$angle = $frame*7*pi()/180;
+
+$geometry = new CubeGeometry(1, ['yellow', 'red', 'white', 'blue', 'orange', 'green']);
+$geometry->rotate($angle,-$angle,$angle*3);
 $mesh = new Mesh($geometry, '#');
 $scene->addMesh($mesh);
 
-$geometry1 = new Geometry();
-$geometry1->addVertex(new Vec3(0,1,0));
-$geometry1->addVertex(new Vec3(-1,0,0));
-$geometry1->addVertex(new Vec3(1,0,0));
-$geometry1->addFace(new Face(0,1,2,'#'));
-$mesh1 = new Mesh($geometry1, '$');
-$mesh1->setPosition(1,0,2);
-$scene->addMesh($mesh1);
-
-$camera->render($scene, $frame, true, false);
+$camera->render($renderer, $scene, $frame, true);
 ?>
