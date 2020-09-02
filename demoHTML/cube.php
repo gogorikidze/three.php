be64b4e965407af0a278d35031e872c86018e287
Your Name <levan@gogorikidze.com>
2020-09-02 13:36:27 +0400

    added experimental HTML rendering option

--- demoHTML/cube.php
+++ demoHTML/cube.php
@@ -1,5 +1,5 @@
 <?php
-include('../three.php');
+include('../threeHTML.php');
 $frame = $_GET['frame'];
 
 $scene = new Scene();
@@ -28,23 +28,23 @@ function cube($size){
   $geometry->addVertex(new Vec3(-$size,$size,$size));
 
   //top
-  $geometry->addFace(new Face(3,2,0,'5'));
-  $geometry->addFace(new Face(2,1,0,'5'));
+  $geometry->addFace(new Face(3,2,0,'blue'));
+  $geometry->addFace(new Face(2,1,0,'blue'));
   //bottom
-  $geometry->addFace(new Face(7,6,5,'4'));
-  $geometry->addFace(new Face(4,7,5,'4'));
+  $geometry->addFace(new Face(7,6,5,'red'));
+  $geometry->addFace(new Face(4,7,5,'red'));
 
-  $geometry->addFace(new Face(0,5,1,'0'));
-  $geometry->addFace(new Face(0,5,4,'0'));
+  $geometry->addFace(new Face(0,5,1,'yellow'));
+  $geometry->addFace(new Face(0,5,4,'yellow'));
 
-  $geometry->addFace(new Face(4,7,0,'1'));
-  $geometry->addFace(new Face(0,3,7,'1'));
+  $geometry->addFace(new Face(4,7,0,'black'));
+  $geometry->addFace(new Face(0,3,7,'black'));
 
-  $geometry->addFace(new Face(5,6,1,'2'));
-  $geometry->addFace(new Face(6,2,1,'2'));
+  $geometry->addFace(new Face(5,6,1,'grey'));
+  $geometry->addFace(new Face(6,2,1,'grey'));
 
-  $geometry->addFace(new Face(7,6,3,'3'));
-  $geometry->addFace(new Face(6,2,3,'3'));
+  $geometry->addFace(new Face(7,6,3,'white'));
+  $geometry->addFace(new Face(6,2,3,'white'));
 
   return $geometry;
 }
--- index.php
+++ index.php
@@ -3,13 +3,13 @@ include('three.php');
 $frame = $_GET['frame'];
 
 $scene = new Scene();
-$camera = new RasterCamera(new Vec3(0,0.5,5), 50, 200, 3, 30, false);
+$camera = new RayCamera(new Vec3(0,0.5,5), 100, 100, 3, 30, false);
 
 $geometry = new Geometry();
 $geometry->addVertex(new Vec3(0,1,0));
 $geometry->addVertex(new Vec3(-1,0,0));
 $geometry->addVertex(new Vec3(1,0,0));
-$geometry->addFace(new Face(0,1,2,'#'));
+$geometry->addFace(new Face(0,1,2,'$'));
 $mesh = new Mesh($geometry, '#');
 $scene->addMesh($mesh);
 
@@ -17,7 +17,7 @@ $geometry1 = new Geometry();
 $geometry1->addVertex(new Vec3(0,1,0));
 $geometry1->addVertex(new Vec3(-1,0,0));
 $geometry1->addVertex(new Vec3(1,0,0));
-$geometry1->addFace(new Face(0,1,2,'$'));
+$geometry1->addFace(new Face(0,1,2,'#'));
 $mesh1 = new Mesh($geometry1, '$');
 $mesh1->setPosition(1,0,2);
 $scene->addMesh($mesh1);
